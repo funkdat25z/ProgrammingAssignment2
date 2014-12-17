43804518d1ccfa8e01bfb47f5447b0bbc2ae57ac
Dan Tran(FangBook) <dtran@hardtostun.com>
2014-12-17 00:41:04 -0500

    Implementing the makeCacheMatrix constructor and the cacheSolve funcs. Win.
    https://www.youtube.com/watch?v=bLBSoC_2IY8&t=5m50s

--- cachematrix.R
+++ cachematrix.R
@@ -2,13 +2,38 @@
 ## add the ability to cache those results
 
 ## Constructor for a matrix object that caches its results for future reuse
+##  CacheMatrix funcs:
+##    - set - add the non-inverted matrix
+##    - get - retrieve the non-inverted matrix
+##    - setInverse - add the inverted matrix
+##    - getInverse - retrieve the inverted matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) invMatrix <<- inv
+  getInverse <- function() invMatrix
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 ## MAKE IT RAIN CACHE MONEY, YO!
 ## Compute and return the inverse of a CacheMatrix object (or recall from the
 ## cache if the result has already been calculated)
 cacheSolve <- function(x, ...) {
-
+  invMatrix <- x$getInverse()
+  if(!is.null(invMatrix)) {
+    message("Cache hit. Returning stored data...")
+    return(invMatrix)
+  }
+  ## Cache miss. WE'RE DOING IT LIVE...
+  data <- x$get()
+  invMatrix <- solve(data, ...)
+  x$setInverse(invMatrix)
+  invMatrix
 }
